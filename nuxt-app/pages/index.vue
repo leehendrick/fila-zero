3f28ccb6a764163c693d71d727ff7effa36d2da5
leehendrick <imhendrick16@gmail.com>
2024-05-15 11:40:09 +0100

    Changing landing page title

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -260,7 +260,7 @@ const navigation = [
         <h1
           class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"
         >
-          Payments tool for software companies
+          Agende atendimentos sem filas em lojas
         </h1>
         <p
           class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
