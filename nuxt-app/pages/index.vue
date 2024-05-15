3850faf0d33d8fce20391eecde80a25ac61eb907
leehendrick <imhendrick16@gmail.com>
2024-05-15 11:41:37 +0100

    Changing landing page subtitle

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -265,8 +265,9 @@ const navigation = [
         <p
           class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"
         >
-          From checkout to global sales tax compliance, companies around the
-          world use Flowbite to simplify their payment stack.
+          FilaZero é uma plataforma que visa melhorar a experência dos clientes
+          ao permitir que eles agendem horários específicos para serem
+          atendidos.
         </p>
         <a
           href="#"
